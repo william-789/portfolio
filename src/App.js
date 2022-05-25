39bfb770a9a7132947727788b8dd374c528156d2
william-789 <riberackw@gmail.com>
2022-05-25 19:33:14 +0100

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Intro from "./Intro";
+import Navbar from "./components/Navbar";
 import Test from "./components/Test";
 
 export default function App() {
   return (
     <>
       <Intro />
+      <Navbar />
       <Test/>
     </>
   );
